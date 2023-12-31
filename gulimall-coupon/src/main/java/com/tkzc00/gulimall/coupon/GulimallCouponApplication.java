83a3731e220b3c3918413574a30d4559b0b33c51
tkzc00 <2238983030@qq.com>
2023-12-31 13:32:42 +0800

    项目初始结构创建

---
+++ gulimall-coupon/src/main/java/com/tkzc00/gulimall/coupon/GulimallCouponApplication.java
@@ -0,0 +1,13 @@
+package com.tkzc00.gulimall.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallCouponApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GulimallCouponApplication.class, args);
+	}
+
+}
---
+++ gulimall-member/src/test/java/com/tkzc00/gulimall/member/GulimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tkzc00.gulimall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
